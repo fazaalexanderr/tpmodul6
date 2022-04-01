f051ad012a21df099b89447e132e8c3586811802
fzTelU <fazaalexander@student.telkomuniversity.ac.id>
2022-04-01 20:51:46 +0700

    Add project files.

---
+++ tpmodul6_kelompok_7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul6_kelompok_7
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
